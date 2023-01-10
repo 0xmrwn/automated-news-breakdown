7d26de0e72ca6769dc801fdbe14ae352696fbbaa
0xmrwn <niches-anode0k@icloud.com>
2023-01-10 19:44:25 +0100

    added support for google translate

--- src/modules/translater.py
+++ src/modules/translater.py
@@ -1,6 +1,7 @@
 import os
 
 import deepl
+from deep_translator import GoogleTranslator
 
 
 def deepl_translate(original_text: str, target: str, lang_level: str):
@@ -16,3 +17,14 @@ def deepl_translate(original_text: str, target: str, lang_level: str):
         text=original_text, target_lang=target, formality=lang_level
     )
     return result.text
+
+
+def google_translate(original_text: str, target: str):
+    """
+    This function uses the Google Translate API to translate a given
+    text from its original language to a target language. The function
+    returns the translated text as a string.
+    """
+    return GoogleTranslator(source="auto", target=target.lower()).translate(
+        text=original_text
+    )
