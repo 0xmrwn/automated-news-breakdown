100a359edd4b3f47c107ca719eb1f217a39b1ac6
0xmrwn <niches-anode0k@icloud.com>
2023-01-19 20:41:01 +0100

    added translation disabling in config

--- src/interface.py
+++ src/interface.py
@@ -1,6 +1,3 @@
-import tkinter
-import tkinter.messagebox
-
 import customtkinter
 
 from modules import scraper, utils
@@ -11,6 +8,10 @@ customtkinter.set_appearance_mode(
 customtkinter.set_default_color_theme(
     "dark-blue"
 )  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+# Move all widgets +1 column and create new frame to be
+# placed on column 0 for main navigation to select use-case
 
 
 class App(customtkinter.CTk):
@@ -26,7 +27,10 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # create sidebar frame with widgets
+        # configure main side bar
+        # self.main_sidebar_frame = customtkinter.CTkFrame(self, width=40, corner_radius=0)
+
+        # create secondary side bar
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
@@ -77,6 +81,7 @@ class App(customtkinter.CTk):
 
         self.main_button_1 = customtkinter.CTkButton(
             master=self,
+            text="Scrape URL",
             fg_color="transparent",
             border_width=2,
             text_color=("gray10", "#DCE4EE"),
@@ -89,109 +94,61 @@ class App(customtkinter.CTk):
         # create textbox
         self.textbox = customtkinter.CTkTextbox(self, width=250)
         self.textbox.grid(
-            row=1, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+            row=2, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
         )
 
         # create tabview
         self.tabview = customtkinter.CTkTabview(self, width=250)
-        self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.tabview.add("CTkTabview")
-        self.tabview.add("Tab 2")
-        self.tabview.add("Tab 3")
-        self.tabview.tab("CTkTabview").grid_columnconfigure(
+        self.tabview.grid(row=2, column=3, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.tabview.add("Interpreter")
+        self.tabview.add("Visualizer")
+        self.tabview.add("Translater")
+        self.tabview.tab("Interpreter").grid_columnconfigure(
             0, weight=1
         )  # configure grid of individual tabs
-        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Visualizer").grid_columnconfigure(0, weight=1)
 
         self.optionmenu_1 = customtkinter.CTkOptionMenu(
-            self.tabview.tab("CTkTabview"),
+            self.tabview.tab("Interpreter"),
             dynamic_resizing=False,
             values=["Value 1", "Value 2", "Value Long Long Long"],
         )
         self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.combobox_1 = customtkinter.CTkComboBox(
-            self.tabview.tab("CTkTabview"),
-            values=["Value 1", "Value 2", "Value Long....."],
+        self.checkbox_1 = customtkinter.CTkCheckBox(
+            self.tabview.tab("Interpreter"), text="Generate Image"
         )
-        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
+        self.checkbox_1.grid(row=1, column=0, pady=(20, 10), padx=20, sticky="n")
+
         self.string_input_button = customtkinter.CTkButton(
-            self.tabview.tab("CTkTabview"),
-            text="Open CTkInputDialog",
+            self.tabview.tab("Interpreter"),
+            text="Get Text",
             command=self.open_input_dialog_event,
         )
-        self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
+        self.string_input_button.grid(row=3, column=0, padx=20, pady=(60, 10))
         self.label_tab_2 = customtkinter.CTkLabel(
-            self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2"
+            self.tabview.tab("Visualizer"), text="CTkLabel on Visualizer"
         )
         self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
 
-        # create radiobutton frame
-        self.radiobutton_frame = customtkinter.CTkFrame(self)
-        self.radiobutton_frame.grid(
-            row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew"
-        )
-        self.radio_var = tkinter.IntVar(value=0)
-        self.label_radio_group = customtkinter.CTkLabel(
-            master=self.radiobutton_frame, text="CTkRadioButton Group:"
-        )
-        self.label_radio_group.grid(
-            row=0, column=2, columnspan=1, padx=10, pady=10, sticky=""
-        )
-        self.radio_button_1 = customtkinter.CTkRadioButton(
-            master=self.radiobutton_frame, variable=self.radio_var, value=0
-        )
-        self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        self.radio_button_2 = customtkinter.CTkRadioButton(
-            master=self.radiobutton_frame, variable=self.radio_var, value=1
-        )
-        self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-        self.radio_button_3 = customtkinter.CTkRadioButton(
-            master=self.radiobutton_frame, variable=self.radio_var, value=2
-        )
-        self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
-
-        # create checkbox and switch frame
-        self.checkbox_slider_frame = customtkinter.CTkFrame(self)
-        self.checkbox_slider_frame.grid(
-            row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew"
-        )
-        self.checkbox_1 = customtkinter.CTkCheckBox(
-            master=self.checkbox_slider_frame, text="Generate Image"
-        )
-        self.checkbox_1.grid(row=1, column=0, pady=(20, 10), padx=20, sticky="n")
-        self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
-        self.checkbox_2.grid(row=2, column=0, pady=10, padx=20, sticky="n")
-        self.switch_1 = customtkinter.CTkSwitch(
-            master=self.checkbox_slider_frame, command=lambda: print("switch 1 toggle")
-        )
-        self.switch_1.grid(row=3, column=0, pady=10, padx=20, sticky="n")
-        self.switch_2 = customtkinter.CTkSwitch(master=self.checkbox_slider_frame)
-        self.switch_2.grid(row=4, column=0, pady=(10, 20), padx=20, sticky="n")
-
-        # create slider and progressbar frame
-
         # set default values
-        self.checkbox_2.configure()
-        self.switch_2.configure()
-        self.checkbox_1.select()
-        self.switch_1.select()
-        self.radio_button_3.configure()
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
         self.optionmenu_1.set("CTkOptionmenu")
-        self.combobox_1.set("CTkComboBox")
         self.textbox.insert(
             "0.0",
             "CTkTextbox\n\n"
             + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n"
-            * 20,
+            * 4,
         )
 
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(
-            text="Type in a number:", title="CTkInputDialog"
-        )
-        print("CTkInputDialog:", dialog.get_input())
+    def open_input_dialog_event(self):  # sourcery skip: class-extract-method
+        dialog = customtkinter.CTkInputDialog(text="Enter URL", title="Scrape")
+        global_config = utils.get_config()
+        goose_config = global_config["goose"]
+        self.textbox.delete(index1="0.0", index2="end")
+        url = dialog.get_input()
+        scraped_data = scraper.scrape_article(url, config=goose_config)
+        self.textbox.insert(index="0.0", text=scraped_data["text"])
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -205,7 +162,7 @@ class App(customtkinter.CTk):
 
     def input_text(self):
         global_config = utils.get_config()
-        goose_config = global_config["goose_config"]
+        goose_config = global_config["goose"]
         self.textbox.delete(index1="0.0", index2="end")
         url = self.entry.get()
         scraped_data = scraper.scrape_article(url, config=goose_config)
